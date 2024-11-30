c33b4c73c4c9321cf914d54cba5ac49be2c94de6
PoPo <Premyslovapolina@gmail.com>
2024-11-30 11:56:54 +0200

    delete user&pass from db config

--- backend/app.py
+++ backend/app.py
@@ -33,8 +33,8 @@ def get_db():
         db = g._database = psycopg2.connect(
             host=os.environ["DB_HOST"],
             database=os.environ["DB_URL"],
-            user=os.environ["DB_USERNAME"],
-            password=os.environ["DB_PASSWORD"],
+            # user=os.environ["DB_USERNAME"],
+            # password=os.environ["DB_PASSWORD"],
         )
     return db
 
